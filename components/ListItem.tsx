b845f693aca82bff3bdb274a84f1bcfcc09ee01a
William Chang <wilicw1346@gmail.com>
2022-07-11 16:48:52 +0800

    Implement list components

---
+++ components/ListItem.tsx
@@ -0,0 +1,21 @@
+import style from '../styles/Home.module.css';
+import Link from 'next/link';
+import Image from 'next/image'
+
+interface ListProp {
+  id: string,
+  name: string,
+  image: string,
+};
+
+const StoreItem = ({ id, image, name }: ListProp) => (
+  <Link href={`/${id}`} >
+    <div className={style.card}>
+      <Image src={image} alt={name} width={100} height={100} />
+      <h1>{name}</h1>
+    </div>
+  </Link>
+)
+
+
+export default StoreItem;
